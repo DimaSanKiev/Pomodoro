4a312ac64799b69c588d31c332840891dc9d3c27
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-09-04 00:42:18 +0300

    add layout Thymeleaf template

--- src/main/java/pomodoro/web/controller/PomodoroController.java
+++ src/main/java/pomodoro/web/controller/PomodoroController.java
@@ -2,9 +2,6 @@ package pomodoro.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import pomodoro.model.Pomodoro;
 import pomodoro.service.PomodoroService;
 
 @Controller
@@ -13,11 +10,4 @@ public class PomodoroController {
     @Autowired
     private PomodoroService pomodoroService;
 
-    @RequestMapping({"/", "/pomodoro"})
-    public String pomodorosList(Model model) {
-        Iterable<Pomodoro> pomodoros = pomodoroService.findAll();
-        model.addAttribute("pomodoros", pomodoros);
-        model.addAttribute("newPomodoro", new Pomodoro());
-        return "pomodoro";
-    }
 }
